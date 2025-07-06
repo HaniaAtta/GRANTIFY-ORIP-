dcc97f515e76989ec6761d8cc7d6fe9bf04da206
hadijalal678 <143265968+hadijalal678@users.noreply.github.com>
2025-07-07 04:22:42 +0500

    Update reliefarc_scraper.py

--- scrapers/selenium_scrapper/reliefarc_scraper.py
+++ scrapers/selenium_scrapper/reliefarc_scraper.py
@@ -1,4 +1,4 @@
-"https://relief-arc.com/partnerships.html"
+
 
 
 
@@ -58,7 +58,7 @@ def is_grant_open(text):
     return True  # fallback: open keyword present, but no past deadline found
 
 # === Scraper ===
-def scrape_wellcome(url="https://wellcome.org/research-funding/schemes"):
+def scrape_wellcome(url="https://relief-arc.com/partnerships.html"):
     options = Options()
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
