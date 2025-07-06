e0c14915667a05f7605c22b5d5512d6f59ecabb8
hadijalal678 <143265968+hadijalal678@users.noreply.github.com>
2025-07-07 04:20:30 +0500

    Update lionsclubs_scraper.py

--- scrapers/selenium_scrapper/lionsclubs_scraper.py
+++ scrapers/selenium_scrapper/lionsclubs_scraper.py
@@ -1,4 +1,4 @@
-"https://www.lionsclubs.org/en"
+
     
 
 import logging
@@ -57,7 +57,7 @@ def is_grant_open(text):
     return True  # fallback: open keyword present, but no past deadline found
 
 # === Scraper ===
-def scrape_wellcome(url="https://wellcome.org/research-funding/schemes"):
+def scrape_wellcome(url="https://www.lionsclubs.org/en"):
     options = Options()
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
