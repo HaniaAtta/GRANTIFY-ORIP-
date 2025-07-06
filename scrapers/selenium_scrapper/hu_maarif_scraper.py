74338a97fd778e2331a593e5cc4c86a5ea0ee950
hadijalal678 <143265968+hadijalal678@users.noreply.github.com>
2025-07-07 04:17:56 +0500

    Update hu_maarif_scraper.py

--- scrapers/selenium_scrapper/hu_maarif_scraper.py
+++ scrapers/selenium_scrapper/hu_maarif_scraper.py
@@ -1,4 +1,4 @@
-"https://hu.maarifschool.org/page/the-turkish-maarif-foundation"
+
     
   import logging
 import re
@@ -56,7 +56,7 @@ def is_grant_open(text):
     return True  # fallback: open keyword present, but no past deadline found
 
 # === Scraper ===
-def scrape_wellcome(url="https://wellcome.org/research-funding/schemes"):
+def scrape_wellcome(url="https://hu.maarifschool.org/page/the-turkish-maarif-foundation"):
     options = Options()
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
